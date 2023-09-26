0d64b0877e4386dfb18f5b69d7534038324299e4
JuanAlejandroSanchezDorantes <143225958+JuanAlejandroSanchezDorantes@users.noreply.github.com>
2023-09-26 00:13:56 -0600

    Update Histograma.py

--- Histograma.py
+++ Histograma.py
@@ -1,37 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Number of simulations
+# Número de simulaciones
 num_simulations = 3000
 
-# Time steps
+# Pasos de tiempo
 n = 10000
 
-# Initialize an array to store the final values of X_n for each simulation
+# Inicializar un array para almacenar los valores finales de X_n para cada simulación.
 final_values = np.zeros(num_simulations)
 
-# Loop through each simulation
+# Recorrer cada simulación
 for i in range(num_simulations):
-    # Initial number of white balls
+    # Número inicial de bolas blancas
     Y_n = 1
-    # Initial total number of balls
+    # Número total inicial de bolas
     total_balls = 1
     
-    # Simulate the process
+    # Simular el proceso
     for t in range(1, n + 1):
-        # Add a white ball with 50% probability, otherwise add a black ball
+        # Añade una bola blanca con 50% de probabilidad, de lo contrario añade una bola negra
         new_ball = np.random.choice(['white', 'black'])
         if new_ball == 'white':
             Y_n += 1
         total_balls += 1
         
-        # Update X_n (martingale)
+        # Actualizar X_n (martingala)
         X_n = Y_n / (total_balls + 1)
     
-    # Store the final value of X_n for this simulation
+    # Almacena el valor final de X_n para esta simulación
     final_values[i] = X_n
 
-# Create the histogram
+# Crear el histograma
 plt.figure(figsize=(10, 6))
 plt.hist(final_values, bins=30, density=True, color='skyblue', edgecolor='black')
 plt.xlabel('$X_{10,000}$')
